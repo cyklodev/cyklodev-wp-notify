f45a3f77cdd84b691f4b7ff1a992b4bf0175d2d9
zephilou <zephilou@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-12 07:57:29 +0000

    tagging version 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/cyklodev-wp-notify/tags/1.2.1@856183 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/notify.php
+++ views/notify.php
@@ -1,6 +1,107 @@
+<div style="position:fixed;right: 10px;bottom:25px;z-index: 0;"><img src="<?php echo plugins_url( 'images/cyklodev.png' , dirname(__FILE__) );?>" width="100px" heigth="100px"/></div>
 <?php
 
 defined('ABSPATH') or die("Cannot access pages directly.");  
+
+/*
+ * Get post ID 
+ */
+
+if(is_numeric($_GET['update_id'])){
+    $post_data = get_post($_GET['update_id']);
+    
+    echo "<hr /><h3>".__("Titre de l'article",'cyklodev')."</h3><center>$post_data->post_title</center><hr />";
+    
+} else {
+    echo '<br />';
+    _e("Notifiez vos utilisateurs par role directement sur vos <a href='edit.php'>posts</a> ou directement dans le post.",'cyklodev');
+    echo '<br />';
+    _e("Pour utiliser les notifications Twitter configurez vos <a href='admin.php?page=cyklodev_notify_twitter'>paramètres</a>.",'cyklodev');
+    return false;
+}
+
+
+if($_GET['twitter'] == 'twitting'){
+ 
+    $options_list = array (
+        'cyklodev_notify_twitter_consumer_secret'          => 'Twitter consumer secret',
+        'cyklodev_notify_twitter_consumer_key'             => 'Twitter consumer key',
+        'cyklodev_notify_twitter_access_token'             => 'Twitter access token',
+        'cyklodev_notify_twitter_access_token_secret'      => 'Twitter access token secret'
+    );
+    
+    $twitter_settings_complete = 1;
+    foreach ($options_list as $k => $v) {
+        if(get_option($k) == ''){
+            $twitter_settings_complete = 0;
+        }
+    }
+    
+    if($twitter_settings_complete == 1){
+         
+        if($_GET['tweet'] == 'true'){
+            echo '<h3>'.__("Notification Twitter",'cyklodev').'</h3>';
+            require_once(plugin_dir_path( dirname(__FILE__) ).'/lib/codebird.php');
+
+            $codebird = new Codebird();
+
+            $codebird->setConsumerKey(get_option('cyklodev_notify_twitter_consumer_key'), get_option('cyklodev_notify_twitter_consumer_secret'));
+            $cb = $codebird->getInstance();
+            $cb->setToken(get_option('cyklodev_notify_twitter_access_token'), get_option('cyklodev_notify_twitter_access_token_secret'));
+            
+            $blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
+            
+            $formated_tweet = $_POST['cyklodev_notify_tweet'];
+            $formated_tweet = preg_replace( "/POST_TITLE/", $post_data->post_title, $formated_tweet );
+            $formated_tweet = preg_replace( "/POST_URL/", get_permalink($_GET['update_id']), $formated_tweet );
+            $formated_tweet = preg_replace( "/BLOG_NAME/", $blogname, $formated_tweet );
+            
+            $params = array(
+                    'status' => $formated_tweet
+            );
+            $reply = $cb->statuses_update($params);
+
+            switch ($reply->httpstatus) {
+                case 401:
+                    echo '<div style="background-color:#ff0000;" align="center">Error : <b>'.$reply->errors[0]->message.'</b> Code ('.$reply->errors[0]->code.')
+                      <br /> '.__("Verifiez  <a href='admin.php?page=cyklodev_notify_twitter'>vos clés twitter !</a>",'cyklodev').' 
+                      </div>';
+                    break;
+                case 403:
+                      echo '<div style="background-color:#ff0000;" align="center">Error : <b>'.$reply->errors[0]->message.'</b> Code ('.$reply->errors[0]->code.')
+                      <br /> '.__("<a href='admin.php?page=cyklodev_notify&update_id=".$_GET['update_id']."&twitter=twitting'>Go Back !</a>",'cyklodev').' 
+                      </div>';
+                    break;
+                 default:
+                    echo '<div style="background-color:#00ff00;" align="center">'.__("Succès !",'cyklodev').'</div>';
+                    break;
+            }
+        } else {
+            if(get_bloginfo('language') == 'fr_FR'){
+                $default_notify_tweet = "Un nouvel article est disponible sur #BLOG_NAME : POST_TITLE POST_URL";
+             } else {
+                $default_notify_tweet = "A new article is available on #BLOG_NAME : POST_TITLE POST_URL";
+             }
+
+            echo '
+                  <h3>'.__("Customisez le tweet",'cyklodev').'</h3>
+                <center>
+                <form action="'.$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING'].'&tweet=true" method="post">
+                <input type="text" name="cyklodev_notify_tweet" id="cyklodev_notify_tweet" size="100" value="'.$default_notify_tweet.'">
+                <input type="submit" value="'.__('Tweet it','cyklodev').'" class="button" />
+                </form>
+                <br /><b>Tips</b> : Metawords are POST_TITLE , POST_URL, BLOG_NAME </center>
+                <hr/>';
+        }
+        
+        
+        return false;
+    } else {
+        _e("Vous devez parametrer <a href='admin.php?page=cyklodev_notify_twitter'>vos clés twitter !</a>",'cyklodev');
+        return false;
+    }
+}
+
 
 /*
  * Get roles 
@@ -20,20 +121,7 @@ foreach($_POST as $key => $value){
     }   
 }
 
-/*
- * Get post ID 
- */
 
-if(is_numeric($_GET['update_id'])){
-    $post_data = get_post($_GET['update_id']);
-    
-    echo "<hr /><h3>".__("Titre de l'article",'cyklodev')."</h3><center>$post_data->post_title</center><hr />";
-    
-} else {
-    echo '<br />';
-    _e("Notifiez vos utilisateurs par role directement sur vos <a href='edit.php'>posts</a> ou directement dans le post.",'cyklodev');
-    return false;
-}
 
 
 foreach ($roles as $k => $v) {
@@ -144,4 +232,4 @@ foreach ($roles as $k => $v) {
 }
 echo '</table></form>';
 ?>
-<div style="position:fixed;right: 10px;bottom:25px;z-index: 0;"><img src="<?php echo plugins_url( 'images/cyklodev.png' , dirname(__FILE__) );?>" width="100px" heigth="100px"/></div>
+
